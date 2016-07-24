99909db59a977c60b47b69afb4802f318c29bb20
rodk <rod2010rod2010@hotmail.com>
2016-07-23 23:27:56 -0400

    added 'Angular Services' to controller

--- app.js
+++ app.js
@@ -2,26 +2,19 @@ var app = angular.module('flapperNews', []);
 
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
-  $scope.test = 'Hello world!';
-//   $scope.posts = [
-//   'post 1',
-//   'post 2',
-//   'post 3',
-//   'post 4',
-//   'post 5'
-// ];
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+'posts',
+function($scope, posts){
+  $scope.test = 'This is a practice app!';
 
-  // $scope.addPost = function(){
-  //   $scope.posts.push({title: 'A new post!', upvotes: 0});
-  // };
+  $scope.posts = posts.posts;
+  
+  $scope.posts = [
+    {title: 'post1', upvotes: 5},
+    {title: 'post2', upvotes: 2},
+    {title: 'post3', upvotes: 15},
+    {title: 'post4', upvotes: 9},
+    {title: 'post5', upvotes: 4}
+  ];
 
   // $scope.title below enables custom titles from html input
   $scope.addPost = function(){
@@ -40,6 +33,13 @@ function($scope){
     post.upvotes += 1;
   };
 
+}]).
+
+factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
 }]);
 
 
